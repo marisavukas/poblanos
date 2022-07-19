d400e172fc9d82dae4cbc42f4b39dea31196c8b5
marisavukas <mvukas98@gmail.com>
2022-07-18 23:56:14 -0400

    changed order to cart

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,59 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import Basket from '../Basket/Basket';
-import BuildOrder from '../BuildOrder/BuildOrder';
+import React, { useEffect, useState } from "react";
+import Basket from "../Basket/Basket";
+import BuildOrder from "../BuildOrder/BuildOrder";
 
-import './Checkout.css';
+import "./Checkout.css";
 
 const Checkout = (props) => {
   const [bagItems, setBagItems] = useState([]);
-  const {mealItems}=props;
-  const {bagArr, mealArrString } = props;
+  const { mealItems } = props;
+  const { bagArr, mealArrString } = props;
 
+  const bagArrString = window.localStorage.getItem(
+    "build-order-items",
+    JSON.stringify(bagArr)
+  );
+  const parsedBagArrString = JSON.parse(bagArrString);
+  // console.log(parsedBagArrString);
 
+  const mealString = window.localStorage.getItem(
+    "meal-selected",
+    JSON.stringify(mealArrString)
+  );
+  const parsedMealString = JSON.parse(mealString);
+  // console.log(parsedMealString);
 
-  const bagArrString= window.localStorage.getItem('build-order-items', JSON.stringify(bagArr));
-  const parsedBagArrString= JSON.parse(bagArrString);
-    // console.log(parsedBagArrString);
+  localStorage.setItem(
+    "cart",
+    JSON.stringify([...parsedMealString, ...parsedBagArrString])
+  );
 
-
-
-    const mealString= window.localStorage.getItem('meal-selected', JSON.stringify(mealArrString));
-    const parsedMealString= JSON.parse(mealString);
-    // console.log(parsedMealString);
-
-    localStorage.setItem('order', JSON.stringify([...parsedMealString, ...parsedBagArrString]));
-
-  
-  
   return (
-    <div className="checkout_page"> 
+    <div className="checkout_page">
       <h1 className="checkout_header">Checkout</h1>
       <hr className="build_order_hr"></hr>
 
-      
       <div className="checkout_page_section">
-      <div className="meal_from_user_contents">
-        <div className="meal_title">
+        <div className="meal_from_user_contents">
+          <div className="meal_title">
+            {parsedMealString.map((item) => (
+              <div key={item.id} className="row">
+                <div className="row-2">{item.name}</div>
+              </div>
+            ))}
+          </div>
 
-          {parsedMealString.map((item) => (
-            <div key={item.id} className="row">
-              <div className="row-2">{item.name}</div>
-
-            </div>
-            
-          ))}
-
-        </div>
-        
-        <div className="meal_ingredients"> 
-          {parsedBagArrString.map((item) => (
-            <div key={item.id} className="row">
-              <div className="row-2">-{item.name}</div>
-            </div>))}
+          <div className="meal_ingredients">
+            {parsedBagArrString.map((item) => (
+              <div key={item.id} className="row">
+                <div className="row-2">-{item.name}</div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      </div>
-      
+
       {/* <div className="row-2">
           <button className="remove">
             -
@@ -63,8 +62,6 @@ const Checkout = (props) => {
           </button>
       </div> */}
       {/* <div className="checkout_items">{checkout_items}</div> */}
-      
-
 
       {/* <div className="checkout_beans">{checkout_beans}</div>
 
@@ -75,12 +72,9 @@ const Checkout = (props) => {
 
 
       {checkout_protein.length === 0 && <div>Cart is empty</div>} */}
-
-
-      
     </div>
-  )
-}
+  );
+};
 
 export default Checkout;
 
@@ -97,8 +91,4 @@ export default Checkout;
 //   }
 // }, []);
 
-
-
-
 // localStorage.setItem('saved-cart-items', JSON.stringify([...newData,...bagItems]));
-
