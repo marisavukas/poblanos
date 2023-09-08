76748dee532c5013d55fada8e5dba6942f141ce6
Marisa Vukas <mvukas98@gmail.com>
2023-09-07 22:34:29 -0400

    refactor footer with styled components

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
-import './Footer.css';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import styled from "styled-components";
+
+import "./Footer.css";
+
+const FooterContainer = styled.div`
+  position: relative;
+  z-index: 1;
+  font-family: var(--font-long);
+  text-transform: uppercase;
+  color: white;
+  font-size: 1.1rem;
+  font-weight: 100;
+  letter-spacing: 0.05em;
+  background: var(--color-black);
+  text-align: center;
+  padding: 1rem 1rem 2rem 1rem;
+  margin: auto;
+  height: 100%;
+`;
+
+const FooterIcons = styled.div`
+  margin-top: 0.5rem;
+
+  svg {
+    color: var(--color-white);
+    margin: 0.5rem;
+    font-size: 24px;
+    cursor: pointer;
+    &:hover {
+      color: var(--color-grey);
+    }
+  }
+`;
+
+const FooterCopyright = styled.p`
+  margin-top: 0.75rem;
+  margin-bottom: 0;
+`;
+
 const Footer = () => (
-    <div className="app__footer section__padding" id="login">
+  <FooterContainer>
+    <FooterIcons>
+      <FiFacebook />
+      <FiTwitter />
+      <FiInstagram />
+    </FooterIcons>
 
-    <div className="app__footer-links">
-    
-
-      <div className="app__footer-links_logo">
-        {/* <img src={images.gericht} alt="footer_logo" />
-        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} /> */}
-        <div className="app__footer-links_icons">
-          <FiFacebook />
-          <FiTwitter />
-          <FiInstagram />
-        </div>
-      </div>
-
-    </div>
-
-    <div className="footer__copyright">
-      <p>2022 Poblano's Grill. All Rights reserved.</p>
-    </div>
-
-  </div>
+    <FooterCopyright>
+      2022 Poblano's Grill. All Rights reserved.
+    </FooterCopyright>
+    {/* </div> */}
+  </FooterContainer>
 );
 
 export default Footer;
